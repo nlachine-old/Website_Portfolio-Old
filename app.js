014091a568e25055df58809f9b6f857061363b3b
Noah Lachine <noah@ontariotechu.net>
2021-09-08 12:53:55 -0400

    Inital HOME page and responsive nav bar

---
+++ app.js
@@ -0,0 +1,9 @@
+//const navLinks = document.querySelectorAll("a");
+const navContainer = document.querySelector("nav");
+console.log(navContainer);
+// for (let link of navLinks) {
+//   link.addEventListener("click", function () {
+//     //navContainer.style.opacity = 0;
+//     console.log("clicked");
+//   });
+// }
